b3138f3a4f53fbe39f50c911d251d4d5885cbe05
quanghuy6698 <quanghuy6698@gmail.com>
2021-09-06 16:25:54 +0700

    [9/6/2021] update content

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 const googleSearchUrl = "https://www.google.com/search?q=";
 
 /**
@@ -6,14 +5,14 @@ const googleSearchUrl = "https://www.google.com/search?q=";
  * @param searchTxt refer to search keywords
  */
 function searchGoogle(searchTxt) {
-    var redirectUrl = googleSearchUrl + searchTxt;
-    redirect(redirectUrl);
+  let redirectUrl = googleSearchUrl + searchTxt;
+  redirect(redirectUrl);
 }
 
 /**
  * redirect to url
  * @param url refer to url to redirect
  */
- function redirect(url) {
-    window.open(url, "_blank");
+function redirect(url) {
+  window.open(url, "_blank");
 }
