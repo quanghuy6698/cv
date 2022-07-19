a9c964e9be2e29357391a5253bfbc5e70e09d663
unknown <quanghuy6698@gmail.com>
2022-07-19 18:14:07 +0700

    [19/7/2022] fix broken UI: terms IT

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/gh-pages/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/cv/" : "/",
 })
